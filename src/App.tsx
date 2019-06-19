0793791ddc1639af2b6a4b691f15b7bf42f462ae
Matt Taylor <matttmiami15@gmail.com>
2019-06-19 17:57:41 -0400

    creating new screen for cart page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 
 import Home from './screens/Home';
 import Login from './screens/Login';
+import Cart from './screens/Cart';
 import { withNavbar } from './shared/Navbar';
 
 const App: React.FC = () => (
@@ -14,6 +15,7 @@ const App: React.FC = () => (
       <Switch>
         <Route exact={true} path="/" component={withNavbar(Home)} />
         <Route exact={true} path="/login" component={withNavbar(Login)} />
+        <Route exact={true} path="/cart" component={withNavbar(Cart)} />
       </Switch>
     </Router>
   </ThemeProvider>
---
+++ src/screens/Cart/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Cart: React.FC = () => <div>Shopping Cart Stuff</div>;
+
+export default Cart;
