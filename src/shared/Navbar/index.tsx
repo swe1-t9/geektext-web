d27aaea3433566ffc1e22be6ce09a869b955544c
Matt Taylor <matttmiami15@gmail.com>
2019-06-19 19:40:20 -0400

    shopping cart button is now right-aligned

--- src/shared/Navbar/index.tsx
+++ src/shared/Navbar/index.tsx
@@ -1,10 +1,20 @@
 import AppBar from '@material-ui/core/AppBar';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'; 
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { Link, LinkProps } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import React from 'react';
+
+// Used to style react components (such as the Cart button) with CSS styling
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    menuButton: {
+      marginLeft: 'auto',
+    },
+  }),
+);
 
 // required for react-router-dom < 6.0.0
 // see https://github.com/ReactTraining/react-router/issues/6056#issuecomment-435524678
@@ -19,9 +29,18 @@ const withNavbar = (Component: React.FC): React.FC => () => (
         <Typography variant="h6" color="inherit">
           GeekText
         </Typography>
-        <IconButton color="default" id="cart-button" aria-label="View Shopping Cart" component={AdapterLink} to="/cart">
-          <ShoppingCartIcon/>
-        </IconButton>
+        {/* Adds shopping cart button */}
+        <div className={useStyles().menuButton}>
+          <IconButton 
+            color="default" 
+            id="cart-button" 
+            aria-label="View Shopping Cart"
+            edge="end"
+            component={AdapterLink} to="/cart"
+          >
+            <ShoppingCartIcon/>
+          </IconButton>
+        </div>
       </Toolbar>
     </AppBar>
     <Component />
