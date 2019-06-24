f99afc5a939c1502004d42a5c01ac7c3a52e9436
Matt Taylor <matttmiami15@gmail.com>
2019-06-24 19:33:04 -0400

    incorporating feedback from code review; made cartbutton a component

--- src/shared/Navbar/index.tsx
+++ src/shared/Navbar/index.tsx
@@ -1,52 +1,34 @@
 import AppBar from '@material-ui/core/AppBar';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'; 
+import { makeStyles } from '@material-ui/core/styles'; 
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import { Link, LinkProps } from 'react-router-dom';
-import IconButton from '@material-ui/core/IconButton';
 import React from 'react';
+import CartButton from '../CartButton'
 
-// Used to style react components (such as the Cart button) with CSS styling
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    menuButton: {
-      marginLeft: 'auto',
-    },
-  }),
-);
-
-// required for react-router-dom < 6.0.0
-// see https://github.com/ReactTraining/react-router/issues/6056#issuecomment-435524678
-const AdapterLink = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
-  <Link innerRef={ref as any} {...props} />
-));
-
-const withNavbar = (Component: React.FC): React.FC => () => (
+const withNavbar = (Component: React.FC): React.FC => () => {
+  const classes = styles();
+  return(
   <>
     <AppBar position="sticky" color="primary">
       <Toolbar>
         <Typography variant="h6" color="inherit">
           GeekText
         </Typography>
-        {/* Adds shopping cart button.
-            For any other buttons that need to be right-aligned on the navbar,
-            place them in this div. */}
-        <div className={useStyles().menuButton}>
-          <IconButton 
-            color="default" 
-            id="cart-button" 
-            aria-label="View Shopping Cart"
-            edge="end"
-            component={AdapterLink} to="/cart"
-          >
-            <ShoppingCartIcon/>
-          </IconButton>
+        {/* Place any buttons that need to be right-aligned in this div. */}
+        <div className={classes.menuButton}>
+          <CartButton/>
         </div>
       </Toolbar>
     </AppBar>
     <Component />
   </>
-);
+  );
+}
+
+const styles = makeStyles(theme => ({
+  menuButton: {
+      marginLeft: 'auto',
+  },
+}));
 
 export { withNavbar };
