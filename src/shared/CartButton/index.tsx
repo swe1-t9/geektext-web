69a4ae88c525c19019e7261319174b4ee84ed9f9
Matt Taylor <matttmiami15@gmail.com>
2019-06-25 18:38:00 -0400

    formatting with prettifier for more consistent indentation

--- src/shared/CartButton/index.tsx
+++ src/shared/CartButton/index.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Link, LinkProps } from 'react-router-dom';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import { Link, LinkProps } from "react-router-dom";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import IconButton from "@material-ui/core/IconButton";
 
 // required for react-router-dom < 6.0.0
 // see https://github.com/ReactTraining/react-router/issues/6056#issuecomment-435524678
-const AdapterLink = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
-    <Link innerRef={ref as any} {...props} />
-));
+const AdapterLink = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  (props, ref) => <Link innerRef={ref as any} {...props} />
+);
 
 const CartButton = () => (
-        <IconButton 
-        color="default" 
-        id="cart-button" 
-        aria-label="View Shopping Cart"
-        edge="end"
-        component={AdapterLink} to="/cart"
-        >
-        <ShoppingCartIcon/>
-        </IconButton>
+  <IconButton
+    color="default"
+    id="cart-button"
+    aria-label="View Shopping Cart"
+    edge="end"
+    component={AdapterLink}
+    to="/cart"
+  >
+    <ShoppingCartIcon />
+  </IconButton>
 );
 
 export default CartButton;
--- src/shared/Navbar/index.tsx
+++ src/shared/Navbar/index.tsx
@@ -1,34 +1,34 @@
-import AppBar from '@material-ui/core/AppBar';
-import { makeStyles } from '@material-ui/core/styles'; 
-import Typography from '@material-ui/core/Typography';
-import Toolbar from '@material-ui/core/Toolbar';
-import React from 'react';
-import CartButton from '../CartButton'
+import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import React from "react";
+import CartButton from "../CartButton";
 
 const withNavbar = (Component: React.FC): React.FC => () => {
   const classes = styles();
-  return(
-  <>
-    <AppBar position="sticky" color="primary">
-      <Toolbar>
-        <Typography variant="h6" color="inherit">
-          GeekText
-        </Typography>
-        {/* Place any buttons that need to be right-aligned in this div. */}
-        <div className={classes.menuButton}>
-          <CartButton/>
-        </div>
-      </Toolbar>
-    </AppBar>
-    <Component />
-  </>
+  return (
+    <>
+      <AppBar position="sticky" color="primary">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            GeekText
+          </Typography>
+          {/* Place any buttons that need to be right-aligned in this div. */}
+          <div className={classes.menuButton}>
+            <CartButton />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Component />
+    </>
   );
-}
+};
 
 const styles = makeStyles(theme => ({
   menuButton: {
-      marginLeft: 'auto',
-  },
+    marginLeft: "auto"
+  }
 }));
 
 export { withNavbar };
