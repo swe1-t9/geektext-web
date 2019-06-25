80806c71c8a22beeb8bd6ab3f145a6e3eee4fac9
Matt Taylor <matttmiami15@gmail.com>
2019-06-25 19:44:37 -0400

    it's like we're playing code golf here. removed unnecessary stuff

--- src/shared/CartButton/index.tsx
+++ src/shared/CartButton/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { Link, LinkProps } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import IconButton from "@material-ui/core/IconButton";
-
-// required for react-router-dom < 6.0.0
-// see https://github.com/ReactTraining/react-router/issues/6056#issuecomment-435524678
-const AdapterLink = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  (props, ref) => <Link innerRef={ref as any} {...props} />
-);
 
 const CartButton = () => (
   <IconButton
@@ -15,7 +9,7 @@ const CartButton = () => (
     id="cart-button"
     aria-label="View Shopping Cart"
     edge="end"
-    component={AdapterLink}
+    component={Link}
     to="/cart"
   >
     <ShoppingCartIcon />
