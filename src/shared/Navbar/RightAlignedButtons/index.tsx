17bdb904f7532ffa01579599667e2afb16de37ee
Matt Taylor <31743217+mtaylor76@users.noreply.github.com>
2019-06-25 22:30:53 -0400

    Wordmark link to root (#8)
    * creating Wordmark component for us to mess with when we have an actual logo
    
    * adding spacing for buttons and added login button

--- src/shared/Navbar/RightAlignedButtons/index.tsx
+++ src/shared/Navbar/RightAlignedButtons/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import CartButton from "./CartButton";
+import LoginButton from "./LoginButton";
 
 const RightAlignedButtons = () => {
   const classes = styles();
   // Add any right-aligned navbar buttons into the following div
   return (
-    <div className={classes.menuButton}>
+    <div className={classes.menuBar}>
+      <LoginButton />
       <CartButton />
     </div>
   );
 };
 
 const styles = makeStyles(theme => ({
-  menuButton: {
+  menuBar: {
     // Ensures buttons are right-aligned
     marginLeft: "auto"
   }
