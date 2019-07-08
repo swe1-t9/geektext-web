9d3baf7773c12eeba85ee040ce827a57b34a3115
Andres Valdes <andres@valdes.io>
2019-07-07 21:46:58 -0700

    Adds query for hello world from server (#11)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,33 @@
+import { QueryRenderer } from 'react-relay';
+// @ts-ignore
+import graphql from 'babel-plugin-relay/macro';
 import React from 'react';
 
-const Home: React.FC = () => <div>Hello world!</div>;
+import { environment } from '../../graphql/relay';
+import HelloWorldView from './HelloWorldView';
+import { HelloWorldView_helloWorld$ref } from './__generated__/HelloWorldView_helloWorld.graphql';
+
+const HomeQuery = graphql`
+  query HomeQuery {
+    ...HelloWorldView_helloWorld
+  }
+`;
+
+const Home: React.FC = () => (
+  <QueryRenderer
+    environment={environment}
+    query={HomeQuery}
+    variables={{}}
+    render={({ props, error }) => {
+      if (error) return <div>{error.message}</div>;
+      else if (props) {
+        console.log(props);
+        return (
+          <HelloWorldView helloWorld={props as HelloWorldView_helloWorld$ref} />
+        );
+      } else return <div>loading</div>;
+    }}
+  />
+);
 
 export default Home;
