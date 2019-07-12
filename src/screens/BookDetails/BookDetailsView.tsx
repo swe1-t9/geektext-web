0cc9a836d66d5df96b9073647c4a4e623fab13c9
Carolina Uribe <curib017@fiu.edu>
2019-07-12 02:15:22 -0400

    minor changes

--- src/screens/BookDetails/BookDetailsView.tsx
+++ src/screens/BookDetails/BookDetailsView.tsx
@@ -3,8 +3,11 @@ import { createFragmentContainer } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
 import { BookDetailsView_bookDetails } from './__generated__/BookDetailsView_bookDetails.graphql';
 import React from 'react';
-import { Card, CardActionArea, CardMedia, CardContent, Button, Typography, CardActions } from '@material-ui/core';
+import { Card, CardActionArea, CardMedia, CardContent, Button, Typography, CardActions, Grid, Theme, IconButton, Collapse } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+
 
 
 type Props = {
@@ -13,45 +16,94 @@ type Props = {
 
 const BookDetailsView: React.FC<Props> = (props: Props) => {
   const classes = useStyles();
-  return(
-  // <div>{props.bookDetails.title}</div>
-  // TODO: in BookDetailsView after everything else is generated for grpahql, this is last step
-  <Card className={classes.card}>
-    <CardActionArea>
-      <CardMedia
-        className={classes.media}
-        image="https://cdn.britannica.com/s:500x350/21/182021-004-532121B3.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          {props.bookDetails.title}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {props.bookDetails.title}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-      <Button size="small" color="primary">
-        Add To Cart
-        </Button>
-      <Button size="small" color="primary">
-        More
-        </Button>
-    </CardActions>
-  </Card>
-)};
+  const [expanded, setExpanded] = React.useState(false);
 
-const useStyles = makeStyles(
+function handleExpandClick() {
+  setExpanded(!expanded);
+}
+  return (
+    // <div>{props.bookDetails.title}</div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="https://cdn.britannica.com/s:500x350/21/182021-004-532121B3.jpg"
+            // className={classes.expandImage}
+            // onClick={expandImage}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.bookDetails.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {props.bookDetails.title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Add To Cart
+        </Button>
+          <Button size="small" color="primary">
+            More
+        </Button>
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>About the Author</Typography>
+            <Typography paragraph>
+              Author Description
+          </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grid>
+  )
+};
+
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     card: {
       marginTop: 10,
       marginLeft: 10,
-      maxWidth: 325,
+      maxWidth: 600,
+      maxHeight: 600,
       background: '#000000'
     },
     media: {
-      height: 198
+      height: 400
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    expandImage:{
+      height: 700,
+      width: 900
     }
   })
 );
