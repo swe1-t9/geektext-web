51c28fcf2fa39e35801917afd3de1d5511e8b015
curib <curib017@fiu.edu>
2019-07-15 15:48:38 -0700

    Book details (#12)
    * begin working with adding book-details to the front end
    
    * add in some graphql stuff for book-details
    
    * implement graphql for the frontend connection
    
    * minor changes
    
    * got book details working with updated schema
    
    * minor changes
    
    * book-details page directly pulls from the db nicely now bless the world
    
    * on author click link to books by that author, fix scrolling issue, and
    add genre and publishing info

---
+++ src/screens/BookDetails/__generated__/BookDetailsView_bookDetails.graphql.ts
@@ -0,0 +1,93 @@
+/* tslint:disable */
+
+import { ReaderFragment } from "relay-runtime";
+export type BookDetailsView_bookDetails$ref = any;
+export type BookDetailsView_bookDetails = {
+    readonly id: string;
+    readonly author_id: string;
+    readonly title: string;
+    readonly isbn: string;
+    readonly genre: string;
+    readonly publish_year: number;
+    readonly price: number;
+    readonly description: string;
+    readonly cover: string;
+    readonly " $refType": BookDetailsView_bookDetails$ref;
+};
+
+
+
+const node: ReaderFragment = {
+  "kind": "Fragment",
+  "name": "BookDetailsView_bookDetails",
+  "type": "Book",
+  "metadata": null,
+  "argumentDefinitions": [],
+  "selections": [
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "id",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "author_id",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "title",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "isbn",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "genre",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "publish_year",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "price",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "description",
+      "args": null,
+      "storageKey": null
+    },
+    {
+      "kind": "ScalarField",
+      "alias": null,
+      "name": "cover",
+      "args": null,
+      "storageKey": null
+    }
+  ]
+};
+(node as any).hash = 'e754cd48bcc449fc11b1624fda28c21f';
+export default node;
